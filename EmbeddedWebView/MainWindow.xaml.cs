7f618992df67cffc73f462390f9396218736393c
Dominik Peukert <Dominik.Peukert@docuware.com>
2022-04-08 07:58:52 +0200

    Initial commit

---
+++ EmbeddedWebView/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Web.WebView2.Core;
+using Newtonsoft.Json;
+using System;
+using System.Windows;
+
+namespace EmbeddedWebView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly string originalTitle;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            originalTitle = Title;
+
+            webView.Source = new Uri(System.Configuration.ConfigurationManager.AppSettings.Get("Source"));
+            webView.WebMessageReceived += WebView_WebMessageReceived;
+        }
+
+        private void WebView_WebMessageReceived(object sender, CoreWebView2WebMessageReceivedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.WebMessageAsJson))
+            {
+                dynamic message = JsonConvert.DeserializeObject(e.WebMessageAsJson);
+                if (message.action == "setTitle")
+                {
+                    Title = $"{originalTitle} {message.value}";
+                }
+
+                if (message.action == "revertTitle")
+                {
+                    Title = originalTitle;
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Embedded-WebView
+
+## Introduction
+With DocuWare 7.5 we have updated the browser runtime in our .NET applications to [WebView2](https://developer.microsoft.com/en-us/microsoft-edge/webview2/).  
+This change also requires external developers to update their solutions when integrating parts of DocuWare WebClient into their .NET applications.
+
+Using this example, you can see all the necessary changes for your application. Before you start, adjust the 'Source' setting in the application configuration file 'App.config' according to your needs.
